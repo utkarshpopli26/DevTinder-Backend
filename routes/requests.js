9851081a07e5994fc671599d8e0566d8f03c16d0
Utkarsh <utkarsh62@deloitte.com>
2025-05-27 18:14:17 +0530

    receive connection request API

--- routes/requests.js
+++ routes/requests.js
@@ -53,4 +53,44 @@ router.post('/request/send/:status/:toUserId', userAuth, async (req, res) => {
 
 });
 
+router.post('/request/recieve/:status/:requestId', userAuth, async (req, res) => {
+
+    try{
+        const {status, requestId} = req.params;
+
+        const allowed_updates = [
+            "accepted",
+            "rejected"
+        ];
+
+        if (!allowed_updates.includes(status)) {
+            return res.status(400).send("Invalid status type");
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            _id: requestId,
+            toUserId: req.user._id,
+            status: "interested",
+        });
+
+        console.log("Connection Request:", connectionRequest);
+
+        const connection = await ConnectionRequest.findOne(connectionRequest);
+
+        if(!connection){
+            return res.status(404).send("Connection request not found");
+        }
+
+        connectionRequest.status = status;
+
+        res.json({
+            message: "Connection request " + status + " successfully",
+            connectionRequest
+        });
+
+    } catch(err) {
+        return res.status(400).send("Error: " + err.message);
+    }
+});
+
 module.exports = router;
